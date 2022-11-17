c8d584e1aa431f48a1a5036ca34e34018e051ede
eperricone <113130003+eperricone@users.noreply.github.com>
2022-11-17 11:53:19 -0500

    Update employeeAppreciation.py

--- employeeAppreciation.py
+++ employeeAppreciation.py
@@ -11,6 +11,7 @@ class EmployeeAppreciation:
         """utilizing dictionary and attributes to make letter of appreciation"""
         full_name = self.first_name + " " + self.last_name
         information_about_employee1 = {'company_position': 'tufting operator', 'years_of_service': '5 years'}
+        #Dictionary is created: key and values.
         message = full_name.title() + ", you have spent " + information_about_employee1[
             'years_of_service'] + " of service to our company as a " + information_about_employee1[
                       'company_position'] + " and we are incredibly grateful to have had your loyalty and talent throughout the years. Thank you!"
